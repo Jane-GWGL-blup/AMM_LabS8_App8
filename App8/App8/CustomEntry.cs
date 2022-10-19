308ef9645401042297ec951e7d4c8326802ce98e
Nicole Caceres <janenieggcs@gmail.com>
2022-10-18 20:47:04 -0500

    Second commit: Se agregó los ejercicios solicitados

--- App8/App8/CustomEntry.cs
+++ App8/App8/CustomEntry.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Xamarin.Forms;
 
 namespace App8
 {
-    public class CustomEntry
+    public class CustomEntry: Entry
     {
+        public CustomEntry()
+        {
+            this.BackgroundColor = Color.DarkGreen;
+            this.TextColor = Color.White;
+        }
+
     }
 }
